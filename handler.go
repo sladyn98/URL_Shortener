3070e148e81c4d45a3f1d1a86f6a7b57d6a2ea25
Sladyn <gunnerforlife00@gmail.com>
2019-07-15 01:20:16 +0530

    Finished up the MapHandler

--- handler.go
+++ handler.go
@@ -11,8 +11,12 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	
+	return func(w http.ResponseWriter, r *http.Request) {
+		path:= r.URL.RequestURI()
+		redirectedPath :=  pathsToUrls[path]
+		http.Redirect(w,r,redirectedPath,http.StatusSeeOther)
+    }
 }
 
 // YAMLHandler will parse the provided YAML and then return
